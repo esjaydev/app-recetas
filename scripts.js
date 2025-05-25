9030e25924039ae43466050f36b0fe2b9ccaba5d
Santiago Jimenez <123615968+esjaydev@users.noreply.github.com>
2025-05-24 22:19:22 -0700

    implementacion de llenado de inputs

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 let alimentos = {
   Carnes: ["Pollo", "Res", "Cerdo", "Cordero", "Pavo", "Pescado"],
-  Lácteos: ["Leche", "Queso", "Yogur", "Mantequilla", "Crema"],
+  Lácteos: ["Leche", "Queso", "Yogurt", "Mantequilla", "Crema"],
   Frutas: ["Manzana", "Banana", "Naranja", "Uva", "Fresa", "Piña"],
   Vegetales: ["Zanahoria", "Espinaca", "Lechuga", "Brócoli", "Pepino", "Pimiento"],
-  Cereales: ["Arroz", "Trigo", "Avena", "Maíz", "Cebada", "Centeno"],
-  Legumbres: ["Frijoles", "Lentejas", "Garbanzos", "Soya", "Arvejas"],
-  Grasas: ["Aceite de oliva", "Manteca", "Aguacate", "Nueces", "Semillas", "Margarina"]
+  Cereales: ["Arroz", "Harina de Trigo", "Avena", "Harina de Maíz", "Elote", "Cebada"],
+  Legumbres: ["Frijoles", "Lentejas", "Soya", "Chicharos"],
+  Grasas: ["Aceite de oliva", "Manteca", "Aguacate", "Nueces", "Aceite Vegetal"]
 }
+
+let inputCategoria = document.getElementById('input-categoria')
+let inputTiempo = document.getElementById('input-tiempo')
+
 let seccionCarnes = document.getElementById('seccion-carnes')
 let seccionLacteos = document.getElementById('seccion-lacteos')
 let seccionFrutas = document.getElementById('seccion-frutas')
@@ -15,17 +19,60 @@ let seccionCereales = document.getElementById('seccion-cereales')
 let seccionLegumbres = document.getElementById('seccion-legumbres')
 let seccionGrasas = document.getElementById('seccion-grasas')
 
-function mostrarIngredientesInputs(seccion, titulo){
-    
-    alimentos.titulo.forEach(e => {
+function mostrarIngredientesInputs(seccion, tipo){
+    tipo.forEach(e => {      
         const ingrediente = document.createElement('div')
         ingrediente.setAttribute('class', 'ingrediente')
+        
         const ingredienteCheckBox = document.createElement('input')
         ingredienteCheckBox.type = 'checkbox'
         ingredienteCheckBox.setAttribute('id', e)
+        ingredienteCheckBox.setAttribute('class', 'checkboxes-inputs')
+
+        const labelIngrediente = document.createElement('label')
+        labelIngrediente.setAttribute('for', e)
+        labelIngrediente.setAttribute('class', 'input-ingrediente')
+
+        const iconoIngrediente = document.createElement('img')
+        iconoIngrediente.src = `./media/ingredientes/${e}.png`
+        iconoIngrediente.alt = e
+        
+        const nombreIngrediente = document.createElement('span')
+        nombreIngrediente.innerText = e
+        
         ingrediente.appendChild(ingredienteCheckBox)
-        seccion.appendChild(ingrediente)
-    })
+        labelIngrediente.appendChild(iconoIngrediente)
+        labelIngrediente.appendChild(nombreIngrediente)
+        ingrediente.appendChild(labelIngrediente)
+                seccion.appendChild(ingrediente)
+            })
     
 }
-mostrarIngredientesInputs(seccionCarnes, 'Carnes')
+mostrarIngredientesInputs(seccionVegetales, alimentos.Vegetales)
+mostrarIngredientesInputs(seccionCarnes, alimentos.Carnes)
+mostrarIngredientesInputs(seccionLacteos, alimentos.Lácteos)
+mostrarIngredientesInputs(seccionFrutas, alimentos.Frutas)
+mostrarIngredientesInputs(seccionCereales, alimentos.Cereales)
+mostrarIngredientesInputs(seccionLegumbres, alimentos.Legumbres)
+mostrarIngredientesInputs(seccionGrasas, alimentos.Grasas)
+
+let checkboxesInputs = document.querySelectorAll('.checkboxes-inputs')
+const botonMostrarRecetas = document.getElementById('mostrar-recetas')
+botonMostrarRecetas.addEventListener('click', function(){
+  console.log(`Mostrar recetas que se realicen ${inputTiempo.value}`);
+  console.log(`Mostrar recetas para ${inputCategoria.value}`);
+  
+  checkboxesInputs.forEach(e => {
+    if(e.checked == true){
+      console.log(e.id);
+    } 
+  })
+})
+
+// const recetasFiltradas = recetas.filter(receta =>
+//   ingredientesBuscados.every(ingBuscado =>
+//     receta.ingredientesObligatorios.some(ing =>
+//       ing.ingrediente === ingBuscado
+//     )
+//   )
+// );
