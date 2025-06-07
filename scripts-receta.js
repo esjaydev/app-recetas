430119fe4fd196ba589bdbd88d2ea235574f28c8
Santiago Jimenez <123615968+esjaydev@users.noreply.github.com>
2025-06-06 23:21:07 -0700

    funcion de obtener receta en pagina

--- scripts-receta.js
+++ scripts-receta.js
@@ -1,8 +1,28 @@
-let displayReceta = document.getElementById('recetaDisplay')
-const queryString = window.location.search;
+const urlParams = new URLSearchParams(window.location.search)
+const myParam = urlParams.get('r')
 
-console.log(queryString);
+async function obtenerReceta(urlID) {
+    let titulo = document.getElementById('titulo')
+    let tiempo = document.getElementById('tiempo')
+    let ingredientes = document.getElementById('ingredientes')
+    try {
+        const response = await fetch('./recetas.json')
+        const recetas = await response.json()
+        recetas.forEach(e => {
+            if (e.codigo == urlID) {
+                titulo.innerText = e.titulo
+                tiempo.innerText = e.tiempoPreparacion
+                e.ingredientes.forEach(ing => {
+                    const elem = document.createElement('li')
+                    elem.innerText = ing.ingredienteTitulo
+                    ingredientes.appendChild(elem)
+                })
+            }
 
-const urlParams = new URLSearchParams(queryString);
-const receta = urlParams.get('receta')
-displayReceta.innerText = receta
+        });
+
+    } catch (error) {
+        console.error('Error cargando la receta:', error)
+    }
+}
+obtenerReceta(myParam)
