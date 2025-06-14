6de2f7578ef1180d75f6d579b809565cb04d00f1
Santiago Jimenez <123615968+esjaydev@users.noreply.github.com>
2025-06-13 23:43:35 -0700

    modo obscuro y claro

--- scripts-receta.js
+++ scripts-receta.js
@@ -1,28 +1,41 @@
-const urlParams = new URLSearchParams(window.location.search)
-const myParam = urlParams.get('r')
+const urlParams = new URLSearchParams(window.location.search);
+const idParam = urlParams.get('r');
 
 async function obtenerReceta(urlID) {
-    let titulo = document.getElementById('titulo')
-    let tiempo = document.getElementById('tiempo')
-    let ingredientes = document.getElementById('ingredientes')
-    try {
-        const response = await fetch('./recetas.json')
-        const recetas = await response.json()
-        recetas.forEach(e => {
-            if (e.codigo == urlID) {
-                titulo.innerText = e.titulo
-                tiempo.innerText = e.tiempoPreparacion
-                e.ingredientes.forEach(ing => {
-                    const elem = document.createElement('li')
-                    elem.innerText = ing.ingredienteTitulo
-                    ingredientes.appendChild(elem)
-                })
-            }
+    const titulo = document.getElementById('titulo');
+    const tiempo = document.getElementById('tiempo');
+    const ingredientes = document.getElementById('ingredientes');
 
-        });
+    // Limpiar contenido anterior
+    titulo.innerText = '';
+    tiempo.innerText = '';
+    ingredientes.innerHTML = '';
+
+    try {
+        const response = await fetch('./recetas.json');
+        const recetas = await response.json();
+
+        const receta = recetas.find(e => e.codigo === urlID);
+
+        if (receta) {
+            titulo.innerText = receta.titulo;
+            tiempo.innerText = `${receta.tiempoPreparacion} minutos`;
+
+            receta.ingredientes.forEach(ing => {
+                const li = document.createElement('li');
+                li.innerText = `${ing.ingredienteTitulo} (${ing.porcion} ${ing.tipoDePorcion})`;
+                ingredientes.appendChild(li);
+            });
+        } else {
+            titulo.innerText = 'Receta no encontrada';
+        }
 
     } catch (error) {
-        console.error('Error cargando la receta:', error)
+        console.error('Error cargando la receta:', error);
+        titulo.innerText = 'Error al cargar la receta';
     }
 }
-obtenerReceta(myParam)
+
+window.onload = function () {
+    obtenerReceta(idParam);
+}
